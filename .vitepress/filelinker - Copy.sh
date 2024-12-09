952833605567ee393d5a1d5f041f1c9afd906ab0
JuckyLee668 <2794920709@qq.com>
2024-12-09 20:29:05 +0800

    change mkfilelist

--- .vitepress/filelinker - Copy.sh
+++ .vitepress/filelinker - Copy.sh
@@ -37,15 +37,15 @@ temp_file="temp.js"
 
 > "$temp_file"
 
-function mklist(){
-    directory="../$1"
+function docs(){
+    directory="../docs"
 
     files=("$directory"/*.md)
     file_count=${#files[@]} # 文件总数
 
 
     # 生成内容到临时文件
-    echo "'/$1/': [" >> "$temp_file"
+    echo "'/docs/': [" >> "$temp_file"
     echo "  {" >> "$temp_file"
     echo "    text: 'Docs'," >> "$temp_file"
     echo "    items: [" >> "$temp_file"
@@ -55,7 +55,7 @@ function mklist(){
     if [ -f "$file" ]; then
         filename=$(basename "$file") # 提取文件名
         name_without_ext="${filename%.*}" # 去掉扩展名
-        link="/$1/${name_without_ext}" # 拼接 link
+        link="/docs/${name_without_ext}" # 拼接 link
 
         # 提取第一个 # 后的标题
         first_heading=$(grep -m 1 '^# ' "$file" | sed 's/^# //' | tr -d '\r')
@@ -78,7 +78,47 @@ function mklist(){
     echo "]," >> "$temp_file"
 }
 
+function tools(){
+    directory="../tools"
 
+    files=("$directory"/*.md)
+    file_count=${#files[@]} # 文件总数
+
+
+    # 生成内容到临时文件
+    echo "'/tools/': [" >> "$temp_file"
+    echo "  {" >> "$temp_file"
+    echo "    text: 'Tools'," >> "$temp_file"
+    echo "    items: [" >> "$temp_file"
+
+    for ((i = 0; i < file_count; i++)); do
+    file="${files[i]}"
+    if [ -f "$file" ]; then
+        filename=$(basename "$file") # 提取文件名
+        name_without_ext="${filename%.*}" # 去掉扩展名
+        link="/tools/${name_without_ext}" # 拼接 link
+
+        # 提取第一个 # 后的标题
+        first_heading=$(grep -m 1 '^# ' "$file" | sed 's/^# //' | tr -d '\r')
+
+
+        # 如果没有找到标题，则使用文件名作为 text
+        text="${first_heading:-$name_without_ext}"
+
+        if ((i == file_count - 1)); then
+        # 最后一个文件
+        echo "      { text: '$text', link: '$link' }" >> "$temp_file"
+        else
+        # 普通文件
+        echo "      { text: '$text', link: '$link' }," >> "$temp_file"
+        fi
+    fi
+    done
+
+    echo "    ]" >> "$temp_file"
+    echo "  }" >> "$temp_file"
+    echo "]" >> "$temp_file"
+}
 function etitle(){
     echo "    
     },
@@ -97,8 +137,8 @@ function etitle(){
 }
 
 ftitle
-mklist docs
-mklist tools
+docs
+tools
 etitle
 
 target_file="config.mts"
